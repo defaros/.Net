507995841f2ec31a75287af40eef5ca5e193658b
defaros <alexandre.honignorth@viacesi.fr>
2014-05-14 15:14:27 +0200

    Composant_Acces_Donnees

--- Ways/Ways/Middleware/7.Service etendu/Composant d'acces aux donnees/CAD.cs
+++ Ways/Ways/Middleware/7.Service etendu/Composant d'acces aux donnees/CAD.cs
@@ -1,27 +1,100 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Ways.Middleware.Service_etendu.Composant_d_acces_aux_donnees
 {
     class CAD
     {
-        //Décris l'accès à la BDD
 
-        public bool openConnectionBDD()
+        private string rq_sql;
+        private string rows;
+        private SqlCommand oCmd;
+        private DbDataAdapter oDA;
+        private DataSet oDS;
+        private SqlConnection oConn;
+        private Hashtable oParameters;
+
+        public CAD()
         {
-            //ouvre une connexion à la BDD
-
-            return true;
+            this.rq_sql = null;
+            this.rows = null;
+            this.oCmd = null;
+            this.oDA = null;
         }
 
-        public bool closeConnectionBDD()
+        public void Connection(MSG oMsg)
         {
-            //ferme la connection à la BDD
+            try
+            {
+                XML oXml = new XML();
+                oXml.ReadConfigDecrypted(oMsg);
+                oConn = new SqlConnection();
 
-            return true;
+                oConn.ConnectionString =
+                    @"Data Source=WIN-" + oMsg.GetData("source") + @";Initial Catalog=" + oMsg.GetData("database") + @";Persist Security Info=True;User ID=" + oMsg.GetData("login") + @";Password=" + oMsg.GetData("pwd") + "1";
+                oConn.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("ERREUR:\n" + e.Message);
+            }
+        }
+
+        // Fonction qui gère les requêtes SELECT
+        public MSG GetRows(MSG oMsg)
+        {
+            Connection(oMsg);
+            this.rq_sql = (string)oMsg.GetData("sql");
+            //this.rows = (string)oMsg.GetData("rows");
+            this.oCmd = new SqlCommand(rq_sql, oConn);
+            this.oDS = new DataSet();
+            //this.oCmd = oConn.CreateCommand();
+
+            if ((Boolean)oMsg.GetData("boolParam"))
+            {
+                oParameters = (Hashtable)oMsg.GetData("parameters");
+
+                foreach (string param in oParameters.Keys)
+                {
+                    oCmd.Parameters.AddWithValue(param, oParameters[param]);
+                }
+            }
+            this.oDA = new SqlDataAdapter(this.oCmd);
+            this.oDA.Fill(this.oDS);
+            oMsg.SetData("dataset", this.oDS);
+            return oMsg;
+        }
+
+        // Fonction qui gère les requêtes INSERT UPDATE ET DELETE
+        public MSG ActionRows(MSG oMsg)
+        {
+            Connection(oMsg);
+            this.rq_sql = (string)oMsg.GetData("sql");
+            this.oCmd = new SqlCommand(rq_sql, oConn);
+
+            if ((Boolean)oMsg.GetData("boolParam"))
+            {
+                oParameters = (Hashtable)oMsg.GetData("parameters");
+
+                foreach (string param in oParameters.Keys)
+                {
+                    oCmd.Parameters.AddWithValue(param, oParameters[param]);
+                }
+            }
+            if (this.oCmd.ExecuteScalar() != null)
+            {
+                oMsg.Statut = true;
+            }
+            oConn.Close();
+            return oMsg;
         }
 
 
---
+++ Ways/Ways/Middleware/7.Service etendu/Composant d'acces aux donnees/MailSmtp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ways.Middleware.Service_etendu.Composant_d_acces_aux_donnees
+{
+    class MailSmtp
+    {
+
+        #region Variables de classe
+
+        private MailMessage message;
+        private SmtpClient smtp;
+
+        #endregion
+
+        #region Constructeurs
+
+        public MailSmtp()
+        {
+            message = new MailMessage();
+            smtp = new SmtpClient();
+        }
+
+        #endregion
+
+        #region Méthode
+
+        public MSG send(MSG oMsg)
+        {
+            #region mail_r
+
+            #endregion
+
+            #region mail_e
+
+            message.To.Add((string)oMsg.GetData("mail"));
+            message.Subject = "Mail JPO EXIA";
+            message.From = new System.Net.Mail.MailAddress("defaros67@gmail.com");
+            if ((string)oMsg.GetData("body") == null)
+            {
+                message.Body = "ici le message du destinataire";
+            }
+            else
+            {
+                message.Body = (string)oMsg.GetData("body");
+            }
+
+            smtp.Host = "smtp.orange.fr";
+            smtp.Port = 25;
+            smtp.Send(message);
+
+            return oMsg;
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
